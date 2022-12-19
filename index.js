213ee3037c556eb5e915295944a4ce251ac316c5
nutaptap <nutaptap@gmail.com>
2022-12-19 17:24:16 +0100

    Implemented the collapsible sections in the mobile menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Menus */
+/* Desktop Menus */
 const featuresButton = document.getElementById("features-button");
 const companyButton = document.getElementById("company-button");
 const featuresMenu = document.getElementById("features-menu");
@@ -46,3 +46,58 @@ window.onclick = function (event) {
     companyMenu.classList.add("hide");
   }
 };
+
+/* Mobile Menus */
+
+const hamburgerButton = document.getElementById("hamburger-button");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const hamburgerImage = document.getElementById("button-image");
+let hamburger = "hidden";
+
+function hamburgerClicked() {
+  if (hamburger == "hidden") {
+    hamburger = "visible";
+    hamburgerMenu.classList.remove("hide");
+    hamburgerImage.src = "images/icon-close-menu.svg";
+  } else {
+    hamburger = "hidden";
+    hamburgerMenu.classList.add("hide");
+    hamburgerImage.src = "images/icon-menu.svg";
+  }
+}
+
+hamburgerButton.addEventListener("click", hamburgerClicked);
+
+/* Inside of the hamburger */
+
+const featuresToggle = document.getElementById("features-toggle");
+const companyToggle = document.getElementById("company-toggle");
+const featuresHamburgerMenu = document.getElementById(
+  "features-hamburger-menu"
+);
+const companyHamburgerMenu = document.getElementById("company-hamburger-menu");
+let featuresHamburger = "remove";
+let companyHamburger = "remove";
+
+function featuresToggled() {
+  if (featuresHamburger == "remove") {
+    featuresHamburger = "visible";
+    featuresHamburgerMenu.classList.remove("remove");
+  } else {
+    featuresHamburger = "remove";
+    featuresHamburgerMenu.classList.add("remove");
+  }
+}
+
+function companyToggled() {
+  if (companyHamburger == "remove") {
+    companyHamburger = "visible";
+    companyHamburgerMenu.classList.remove("remove");
+  } else {
+    companyHamburger = "remove";
+    companyHamburgerMenu.classList.add("remove");
+  }
+}
+
+featuresToggle.addEventListener("click", featuresToggled);
+companyToggle.addEventListener("click", companyToggled);
